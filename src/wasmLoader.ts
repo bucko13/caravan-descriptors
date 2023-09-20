a1002076f4021165e089ed5a05f1afb6c5f537f6
buck <buck@unchained.com>
2023-09-20 12:16:18 -0500

    chore: another attempt to fix builds

--- src/wasmLoader.ts
+++ src/wasmLoader.ts
@@ -1,16 +1,14 @@
-import type * as web from "caravan-rs/pkg-web/caravan_rs";
-import type * as node from "caravan-rs/pkg-nodejs/caravan_rs";
+import type * as web from "src/caravan-rs/pkg-web/caravan_rs";
+import type * as node from "src/caravan-rs/pkg-nodejs/caravan_rs";
 
 let bdkWasm: Promise<typeof web | typeof node>;
 
 if (typeof window !== "undefined") {
   // Browser environment
-  bdkWasm = import("../caravan-rs/pkg-web/caravan_rs");
-
+  bdkWasm = import("./caravan-rs/pkg-web/caravan_rs");
 } else {
   // Node.js environment
-  bdkWasm = import("../caravan-rs/pkg-nodejs/caravan_rs");
-
+  bdkWasm = import("./caravan-rs/pkg-nodejs/caravan_rs");
 }
 
 export default bdkWasm;
